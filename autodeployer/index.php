1b9e0ea6aa4bd72f1dbf3d2d93ca6479c8d5e3b8
A.Laptinskaya <A.Laptinskaya@cbt.by>
2026-03-12 14:14:11 +0300

    start

--- autodeployer/index.php
+++ autodeployer/index.php
@@ -14,8 +14,10 @@ $db->initTables();
 
 require_once __DIR__ . '/src/GitClient.php';
 
-$git = new Autodeployer\GitClient("/"); // /home/bitrix/siteProject
+$git = new Autodeployer\GitClient($_SERVER['DOCUMENT_ROOT']);
 
 echo '<pre>';
+var_dump($_SERVER['DOCUMENT_ROOT']);
+var_dump($git->getCurrentGitDir());
 var_dump($git->getCurrentBranch());
 var_dump($git->getRemoteBranches());
