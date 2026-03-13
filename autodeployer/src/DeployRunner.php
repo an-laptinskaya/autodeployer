9303978cde2678c99a39d6f3e506c827baedb063
A.Laptinskaya <A.Laptinskaya@cbt.by>
2026-03-13 13:27:13 +0300

    save logs in file

--- autodeployer/src/DeployRunner.php
+++ autodeployer/src/DeployRunner.php
@@ -15,6 +15,7 @@ class DeployRunner
 
     public function deploy(int $envId, string $strategy = 'commit')
     {
+        $deployStartDate = date('Y-m-d-H-i-s');
         $log = [];
         $dbPrefix = $this->db->getPrefix();
 
@@ -33,63 +34,81 @@ class DeployRunner
         $envName = $environment['name'];
 
         $log[] = "[INFO] Начинаем деплой для: {$envName} (Ветка: {$branch})";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         // Инициализируем GitClient ИМЕННО для папки этой площадки
         $git = new GitClient($targetPath);
 
         $log[] = "Устанавливаем пользователя";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
         $userConfig = $git->setGitUserConfig();
         $log[] = $userConfig['output'];
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         // Стягиваем информацию об изменениях с сервера
         $log[] = "[GIT] git fetch --all...";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
         $git->fetchAll();
 
         if ($strategy === 'reset') {
             $log[] = "[GIT] Стратегия RESET: Жесткий сброс локальных правок...";
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             $res = $git->resetHard();
         } elseif ($strategy === 'stash') {
             $log[] = "[GIT] Стратегия STASH: Прячем локальные правки в карман...";
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             $res = $git->stash();
         } else {
             // По умолчанию работает логика "как в sh.git" (commit)
             $log[] = "[GIT] Стратегия COMMIT: Сохраняем локальные правки на сервере...";
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             $res = $git->commitLocalChanges();
         }
         $log[] = $res['output'];
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         // Обновляем код
         $log[] = "[GIT] git pull origin {$branch}...";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
         $pullResult = $git->pull($branch);
         $log[] = $pullResult['output'];
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         if (!$pullResult['success']) {
             $this->failDeploy($envName, $branch, $log, "Ошибка при выполнении pull");
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             return ['success' => false, 'log' => implode("\n", $log)];
         }
 
         // Переключаемся на нужную ветку
         $log[] = "[GIT] git checkout {$branch}...";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
         $checkoutResult = $git->checkout($branch);
         $log[] = $checkoutResult['output'];
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         if (!$checkoutResult['success']) {
             $this->failDeploy($envName, $branch, $log, "Ошибка при переключении ветки");
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             return ['success' => false, 'log' => implode("\n", $log)];
         }
 
         // ОБРАБОТКА КОНФЛИКТОВ (только для режима commit)
         if ($strategy === 'commit' && !$pullResult['success'] && strpos($pullResult['output'], 'Conflict') !== false) {
             $log[] = "[GIT] Обнаружен конфликт! Решаем автоматически в пользу сервера (--theirs)...";
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             $resolveResult = $git->resolveConflicts();
             $log[] = $resolveResult['output'];
+            $this->log($deployStartDate, $log[array_key_last($log)]);
 
             if (!$resolveResult['success']) {
                 $this->failDeploy($envName, $branch, $log, "Не удалось разрешить конфликты автоматически.");
+                $this->log($deployStartDate, $log[array_key_last($log)]);
                 return ['success' => false, 'log' => implode("\n", $log)];
             }
         } elseif (!$pullResult['success']) {
             $this->failDeploy($envName, $branch, $log, "Ошибка при выполнении pull.");
+            $this->log($deployStartDate, $log[array_key_last($log)]);;
             return ['success' => false, 'log' => implode("\n", $log)];
         }
 
@@ -99,12 +118,15 @@ class DeployRunner
         // Выполнение пост-команд (например, npm ci, сброс кэша)
         if (!empty($environment['build_command'])) {
             $log[] = "[CMD] Выполняем: {$environment['build_command']}";
+            $this->log($deployStartDate, $log[array_key_last($log)]);
             $cmd = sprintf('cd %s && %s 2>&1', escapeshellarg($targetPath), $environment['build_command']);
             exec($cmd, $cmdOutput, $cmdCode);
             $log[] = implode("\n", $cmdOutput);
+            $this->log($deployStartDate, $log[array_key_last($log)]);
         }
 
         $log[] = "[SUCCESS] Деплой успешно завершен!";
+        $this->log($deployStartDate, $log[array_key_last($log)]);
 
         // Отправляем успешное уведомление
 //        $this->notifier->send("<b>Деплой успешно завершен!</b> 🎉\nПлощадка: {$envName}\nВетка: {$branch}");
@@ -224,7 +246,7 @@ class DeployRunner
         ];
     }
 
-    private function failDeploy(string $envName, string $branch, array $log, string $errorReason): void
+    private function failDeploy(string $envName, string $branch, array &$log, string $errorReason): void
     {
         $log[] = "[ERROR] " . $errorReason;
         $errorText = implode("\n", $log);
@@ -232,5 +254,12 @@ class DeployRunner
 //            "<b>🚨 Ошибка деплоя!</b>\nПлощадка: {$envName}\nВетка: {$branch}\n\n<pre>{$errorText}</pre>",
 //            true
 //        );
+    }
+
+    private function log($datetime, $message)
+    {
+        if ($datetime && $message) {
+            file_put_contents(__DIR__ . '/../logs/' . $datetime . '.json', $message . PHP_EOL, FILE_APPEND);
+        }
     }
 }
